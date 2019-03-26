31c1c7c9bd0b1a90a3f35f881962bb6270650b54
Louisejesuis <louise.roy@hotmail.fr>
2019-03-26 13:40:27 +0100

    first test

---
+++ App.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Text, View } from 'react-native';
+import App from './App';
+
+describe('App', () => {
+  it('renders View', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(View)).toHaveLength(1);
+  });
+});
+
+describe('within View', () => {
+  it('renders Text with text "Aucun article"', () => {
+    const wrapper = shallow(<App />);
+    expect(
+      wrapper.find(View).contains(<Text>Aucun article</Text>)
+    ).toBeTruthy();
+  });
+});
